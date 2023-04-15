c6d6f61e3589a4f11e1177a43bda6046fa62adab
alefemoreira <alefe@fab.work>
2023-04-15 16:29:54 -0300

    refatoração de nomes

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from utils.rgb2yiq import rgb2yiq
 dancingInWater = Image.open(('../images/DancingInWater.jpg')).convert('RGB')
 image = np.array(dancingInWater, 'uint8')
 
-output = median_filter(image, size=(9, 9), center=(4, 4), use_zero=True)
+output = median_filter(image, dim=(9, 9), pivot=(4, 4), use_zero=True)
 
 i_output = Image.fromarray(output)
 i_output.show()
