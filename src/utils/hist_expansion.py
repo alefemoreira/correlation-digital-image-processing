d6c55692406fb3b05bd61c5d1eecf54c8f11873c
weverton <wevertonvitor01@gmail.com>
2023-04-18 09:54:07 -0300

    histogram

--- src/utils/hist_expansion.py
+++ src/utils/hist_expansion.py
@@ -6,4 +6,11 @@ def calc_expansion(r, r_min, r_max, l):
     return np.round(((r - r_min) / (r_max - r_min)) * (l - 1))
 
 def histogram_expansion(image: ndarray, l):
+    # pegar o maximo de cada banda
+    # pegar o minimo de cada banda
+    # percorrer a imagem aplixando o calc_expansion nas 3 bandas de cada pixel
+    pass
+
+
+def get_r_min_max(image: ndarray):
     pass
