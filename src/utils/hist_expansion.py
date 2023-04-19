723e574ed96cd293c1a2305988d7b35b9a0e168b
Alysson-Silva <alyssonlogar123@gmail.com>
2023-04-18 22:39:21 -0300

    Update hist_expansion.py

--- src/utils/hist_expansion.py
+++ src/utils/hist_expansion.py
@@ -3,12 +3,17 @@ from numpy import ndarray
 
 
 def calc_expansion(r, r_min, r_max, l):
+
+    #Força número real para garantir resultados matemáticos corretos
+    #Se r_max for um número inteiro, a matemática perderia a precisão
+    r_max = float(r_max)
+
     return np.round(((r - r_min) / (r_max - r_min)) * (l - 1))
 
 def histogram_expansion(image: ndarray, l):
     # pegar o maximo de cada banda
     # pegar o minimo de cada banda
-    # percorrer a imagem aplixando o calc_expansion nas 3 bandas de cada pixel
+    # percorrer a imagem aplicando o calc_expansion nas 3 bandas de cada pixel
     pass
 
 
