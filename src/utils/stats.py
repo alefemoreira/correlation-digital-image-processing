15d4ff84e4d6d923602bfc399d363b75fcd5f2a3
Ruanderson Gabriel <88016092+ruanderson1@users.noreply.github.com>
2023-04-19 23:37:50 -0300

    Update stats.py

--- src/utils/stats.py
+++ src/utils/stats.py
@@ -32,3 +32,14 @@ def fit0_255(value):
     if v > 255: return 255
 
     return v
+
+def normaliza(r,g,b):
+    r = 0 if r < 0 else r
+    r = 255 if r > 255 else r
+
+    g = 0 if g < 0 else g
+    g = 255 if g > 255 else g
+        
+    b = 0 if b < 0 else b
+    b = 255 if b > 255 else b
+    return r,g,b
