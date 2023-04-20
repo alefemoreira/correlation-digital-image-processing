fc0fd00925521e3f8093d12be504f18ad6184952
Ruanderson Gabriel <88016092+ruanderson1@users.noreply.github.com>
2023-04-19 23:36:21 -0300

    Update negative_y.py

--- src/filters/negative_y.py
+++ src/filters/negative_y.py
@@ -1,35 +1,40 @@
-from numpy import ndarray
+import numpy as np 
 from PIL import Image
+
 
 from utils.rgb2yiq import  rgb2yiq
 from utils.yiq2rgb import  yiq2rgb
-from utils import  y_inversion
+from utils.y_inversion import  y_inversion
 from utils.stats import normaliza
 
-def negY(image: ndarray):
+def negY(image: np.ndarray):
   """
     Recebe uma imagem em formato ndarray e retorna esse mesmo ndarray com a conversão aplicada
   """
-
-  negY_image = []
-
-  for line in image:
+  
+  negY_image = np.ndarray.__array__(image, 'uint8')
+  #percorro a imagem
+  for i in range(len(image)):
     new_line = []
-    for pixel in line:
-      r,g,b = pixel[0], pixel[1], pixel[2] 
+    for j in range(len(image[0])):
+      r,g,b = image[i][j] 
 
       # Conversão para YIQ com Y negativo
-      y,i,q = rgb2yiq((r,g,b))
-      # Operação para aplicar o negativo na banda Y
-      y = y-255
-      # Conversão para RGB
-      r,g,b = yiq2rgb((y,i,q))
+      Y,I,Q = rgb2yiq((r,g,b))
+      
+      #operação para aplicar o negativo na banda Y
+      Y2 = 255-Y
+      
+      # Conversao para RGB
+      r,g,b = yiq2rgb((Y2,I,Q))
+      
       # Normalização dos limites
       r,g,b = normaliza(r,g,b)
-      # Novo pixel
+      
+      # Novo linha
       new_line.append([r,g,b])
+      
+      #Add a imagem
+      negY_image[i][j] = [r,g,b]
 
-    # Nova linha
-    negY_image.append(new_line)
-  
   return negY_image
