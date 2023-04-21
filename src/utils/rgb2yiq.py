75423a68f2c47a657ee12800bc0428b70f7a3348
weverton <wevertonvitor01@gmail.com>
2023-04-21 07:28:13 -0300

    fiz rgb2yiq

--- src/utils/rgb2yiq.py
+++ src/utils/rgb2yiq.py
@@ -4,7 +4,7 @@ def rgb2yiq(rgb: tuple) -> tuple:
     r, g, b = rgb
     
     y = ceil(0.299 * r + 0.587 * g + 0.114 * b)
-    i = ceil(0.596 * r - 0.275 * g - 0.321 * b)
-    q = ceil(0.212 * r - 0.523 * g + 0.311 * b)
+    i = ceil(0.596 * r - 0.274 * g - 0.322 * b)
+    q = ceil(0.211 * r - 0.523 * g + 0.312 * b)
 
     return (y, i, q)
