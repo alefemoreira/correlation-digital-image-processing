717fe1eba9fbaa4fd1e1f06add05300951cf7253
weverton <wevertonvitor01@gmail.com>
2023-04-25 09:52:16 -0300

    Comments

--- src/filters/median.py
+++ src/filters/median.py
@@ -6,16 +6,21 @@ def median_filter(image, dim, pivot, use_zero=True):
     m, n = dim
     output = np.array(image, 'uint8')
 
+    # mxn impar
     if m % 2 == 0 or n % 2 == 0 or m <= 0 or n <= 0:
         return None
     
     for i in range(len(image)):
         for j in range(len(image[0])):
+            # Buscando a vizinhaça
             neighbors = neighborsRGB(image, (i, j), dim, pivot, use_zero)
+
+            # Caso haja alguma vizinhaça fora da imagem o pixel não é calculado
             if neighbors == None:
                 output[i][j] = (0, 0, 0)
                 continue
 
+            # Fazendo a mediana para as 3 bandas
             r, g, b = neighbors
             output[i][j][0] = np.median(r)
             output[i][j][1] = np.median(g)
